a3059c45883086eb5ef5614335e64246f57850b7
Yuri Novikov <yuri@freakingloud.net>
2025-11-01 08:52:33 +0300

    added more details 2

--- src/mocks/offers.ts
+++ src/mocks/offers.ts
@@ -14,6 +14,19 @@ export const offers: Offer[] = [
     isBookmarked: false,
     latitude: 52.3909553943508,
     longitude: 4.85309666406198,
+    reviews: [
+      {
+        id: uuid(),
+        user: {
+          name: 'Max',
+          avatar: 'img/avatar-max.jpg',
+        },
+        rating: 4,
+        comment:
+          'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.',
+        date: '2019-04-24',
+      },
+    ],
   },
   {
     id: uuid(),
@@ -26,6 +39,19 @@ export const offers: Offer[] = [
     isBookmarked: true,
     latitude: 52.3609553943508,
     longitude: 4.85309666406198,
+    reviews: [
+      {
+        id: uuid(),
+        user: {
+          name: 'Angelina',
+          avatar: 'img/avatar-angelina.jpg',
+        },
+        rating: 5,
+        comment:
+          'An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.',
+        date: '2019-05-15',
+      },
+    ],
   },
   {
     id: uuid(),
@@ -38,6 +64,7 @@ export const offers: Offer[] = [
     isBookmarked: false,
     latitude: 52.3909553943508,
     longitude: 4.929309666406198,
+    reviews: [],
   },
   {
     id: uuid(),
@@ -50,5 +77,29 @@ export const offers: Offer[] = [
     isBookmarked: false,
     latitude: 52.3809553943508,
     longitude: 4.939309666406198,
+    reviews: [
+      {
+        id: uuid(),
+        user: {
+          name: 'Max',
+          avatar: 'img/avatar-max.jpg',
+        },
+        rating: 5,
+        comment:
+          'The apartment is perfect for a family vacation. Clean, comfortable and very spacious.',
+        date: '2019-06-10',
+      },
+      {
+        id: uuid(),
+        user: {
+          name: 'Angelina',
+          avatar: 'img/avatar-angelina.jpg',
+        },
+        rating: 4,
+        comment:
+          'Great location and beautiful view. The host was very helpful and responsive.',
+        date: '2019-07-02',
+      },
+    ],
   },
 ];
