5c5069d9a9d2d2506f9d87b1aaa0fff5c8583e7f
Yuri Novikov <yuri@freakingloud.net>
2025-11-15 14:11:42 +0300

    refactor api actions

--- src/store/api-actions.ts
+++ src/store/api-actions.ts
@@ -8,46 +8,38 @@ type CommentData = {
   rating: number;
 };
 
-export const fetchOffers = createAsyncThunk<
-  void,
-  undefined,
-  {
-    dispatch: AppDispatch;
-    state: RootState;
-    extra: AxiosInstance;
-  }
->('offers/fetchOffers', async (_arg, { dispatch, extra: api }) => {
-  const { data } = await api.get<Offer[]>('/offers');
+type Config = {
+  dispatch: AppDispatch;
+  state: RootState;
+  extra: AxiosInstance;
+};
 
-  dispatch(setOffers(data));
-});
+export const fetchOffers = createAsyncThunk<void, undefined, Config>(
+  'offers/fetchOffers',
+  async (_arg, { dispatch, extra: api }) => {
+    const { data } = await api.get<Offer[]>('/offers');
 
-export const checkAuth = createAsyncThunk<
-  void,
-  undefined,
-  {
-    dispatch: AppDispatch;
-    state: RootState;
-    extra: AxiosInstance;
+    dispatch(setOffers(data));
   }
->('user/checkAuth', async (_arg, { dispatch, extra: api }) => {
-  try {
-    const { data } = await api.get<AuthInfo>('/login');
-    dispatch(setUser(data));
-    dispatch(setAuthStatus('AUTH'));
-  } catch {
-    dispatch(setAuthStatus('NO_AUTH'));
+);
+
+export const checkAuth = createAsyncThunk<void, undefined, Config>(
+  'user/checkAuth',
+  async (_arg, { dispatch, extra: api }) => {
+    try {
+      const { data } = await api.get<AuthInfo>('/login');
+      dispatch(setUser(data));
+      dispatch(setAuthStatus('AUTH'));
+    } catch {
+      dispatch(setAuthStatus('NO_AUTH'));
+    }
   }
-});
+);
 
 export const login = createAsyncThunk<
   AuthInfo,
   { email: string; password: string },
-  {
-    dispatch: AppDispatch;
-    state: RootState;
-    extra: AxiosInstance;
-  }
+  Config
 >('user/login', async ({ email, password }, { dispatch, extra: api }) => {
   const { data } = await api.post<AuthInfo>('/login', { email, password });
   localStorage.setItem('token', data.token);
@@ -56,53 +48,34 @@ export const login = createAsyncThunk<
   return data;
 });
 
-export const fetchOfferDetails = createAsyncThunk<
-  OfferDetails,
-  string,
-  {
-    dispatch: AppDispatch;
-    state: RootState;
-    extra: AxiosInstance;
+export const fetchOfferDetails = createAsyncThunk<OfferDetails, string, Config>(
+  'offer/fetchDetails',
+  async (offerId, { extra: api }) => {
+    const { data } = await api.get<OfferDetails>(`/offers/${offerId}`);
+    return data;
   }
->('offer/fetchDetails', async (offerId, { extra: api }) => {
-  const { data } = await api.get<OfferDetails>(`/offers/${offerId}`);
-  return data;
-});
+);
 
-export const fetchNearbyOffers = createAsyncThunk<
-  Offer[],
-  string,
-  {
-    dispatch: AppDispatch;
-    state: RootState;
-    extra: AxiosInstance;
+export const fetchNearbyOffers = createAsyncThunk<Offer[], string, Config>(
+  'offer/fetchNearby',
+  async (offerId, { extra: api }) => {
+    const { data } = await api.get<Offer[]>(`/offers/${offerId}/nearby`);
+    return data;
   }
->('offer/fetchNearby', async (offerId, { extra: api }) => {
-  const { data } = await api.get<Offer[]>(`/offers/${offerId}/nearby`);
-  return data;
-});
+);
 
-export const fetchComments = createAsyncThunk<
-  Review[],
-  string,
-  {
-    dispatch: AppDispatch;
-    state: RootState;
-    extra: AxiosInstance;
+export const fetchComments = createAsyncThunk<Review[], string, Config>(
+  'offer/fetchComments',
+  async (offerId, { extra: api }) => {
+    const { data } = await api.get<Review[]>(`/comments/${offerId}`);
+    return data;
   }
->('offer/fetchComments', async (offerId, { extra: api }) => {
-  const { data } = await api.get<Review[]>(`/comments/${offerId}`);
-  return data;
-});
+);
 
 export const submitComment = createAsyncThunk<
   Review,
   { offerId: string; commentData: CommentData },
-  {
-    dispatch: AppDispatch;
-    state: RootState;
-    extra: AxiosInstance;
-  }
+  Config
 >('offer/submitComment', async ({ offerId, commentData }, { extra: api }) => {
   const { data } = await api.post<Review>(`/comments/${offerId}`, commentData);
   return data;
